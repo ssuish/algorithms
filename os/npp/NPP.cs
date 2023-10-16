989b03076d44d8b188c55cb05db708f2c2b5669c
suisoh <fubuki.k016@gmail.com>
2023-10-16 14:58:13 +0800

    constructor

--- os/npp/NPP.cs
+++ os/npp/NPP.cs
@@ -1,2 +1,17 @@
 namespace npp;
 
+public class Process
+{
+    private List<int>P;
+    private List<int>CPULoad;
+    private int time;
+
+    public Process()
+    {
+        P = new List<int>();
+        CPULoad = new List<int>();
+        time = 1;
+    }
+
+
+}
